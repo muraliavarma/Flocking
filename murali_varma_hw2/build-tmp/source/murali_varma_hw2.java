78a111730ae0c32da6e2d629c55407431c7f5084
Murali <muraliavarma@gmail.com>
2013-01-27 18:49:58 -0500

    Fixed indentation in Creature.pde

--- murali_varma_hw2/build-tmp/source/murali_varma_hw2.java
+++ murali_varma_hw2/build-tmp/source/murali_varma_hw2.java
@@ -73,55 +73,55 @@ public void draw() {
 Creature[] creatures;
 
 class Creature {
-  float posX;
-  float posY;
+	float posX;
+	float posY;
 
-  float velX;
-  float velY;
+	float velX;
+	float velY;
 
-  float radius = 2;
+	float radius = 10;
 
-  Creature() {
-    posX = random(1);
-    posY = random(1);
-    velX = 0.01f - random(0.02f);
-    velY = 0.01f - random(0.02f);
-  }
+	Creature() {
+		posX = random(1);
+		posY = random(1);
+		velX = 0.01f - random(0.02f);
+		velY = 0.01f - random(0.02f);
+	}
 
-  public void draw() {
-    arc(SCREEN_WIDTH * posX, SCREEN_HEIGHT * posY, radius, radius, 0, 2 * PI);
-  }
+	public void draw() {
+		arc(SCREEN_WIDTH * posX, SCREEN_HEIGHT * posY, radius, radius, 0, 2 * PI);
+	}
 
-  public void update() {
-    posX += velX;
-    posY += velY;
+	public void update() {
+		posX += velX;
+		posY += velY;
 
-    if (edgeBehavior == TOROIDAL_MODE) {
-      if (posX > 1) {
-        posX = 0;
-      }
-      if (posY > 1) {
-        posY = 0;
-      }
-      if (posX < 0) {
-        posX = 1;
-      }
-      if (posY < 0) {
-        posY = 1;
-      }
-    }
-    else {
-      //reflect mode
-      if (posX > 1 || posX < 0) {
-        velX *= -1;
-        posX += 2 * velX; //to compensate for creature having gone out of screen
-      }
-      if (posY > 1 || posY < 0) {
-        velY *= -1;
-        posY += 2 * velY;
-      }
-    }
-  }
+		if (edgeBehavior == TOROIDAL_MODE) {
+			if (posX > 1) {
+				posX = 0;
+			}
+			if (posY > 1) {
+				posY = 0;
+			}
+			if (posX < 0) {
+				posX = 1;
+			}
+			if (posY < 0) {
+				posY = 1;
+			}
+		}
+		else {
+			//reflect mode
+			if (posX > 1 || posX < 0) {
+				velX *= -1;
+				posX += 2 * velX;	//to compensate for creature having gone out of screen
+			}
+			if (posY > 1 || posY < 0) {
+				velY *= -1;
+				posY += 2 * velY;
+			}
+		}
+	}
 };
 
   static public void main(String[] passedArgs) {
